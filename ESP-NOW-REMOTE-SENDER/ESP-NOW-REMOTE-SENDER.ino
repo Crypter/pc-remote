d20522cc56826fe4b9151f523820ed8d31157daa
Crypter <crypter@mail.com>
2020-10-23 23:17:13 +0200

    deep sleep fixed, sleep impoved

--- ESP-NOW-REMOTE-SENDER/ESP-NOW-REMOTE-SENDER.ino
+++ ESP-NOW-REMOTE-SENDER/ESP-NOW-REMOTE-SENDER.ino
@@ -8,10 +8,8 @@ const uint8_t broadcast_addr[6] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
 uint8_t packet_data[2] = {0, 0};
 
 uint8_t wifi_state = 0;
+uint8_t wifi_sent = 1;
 uint8_t data_to_send = 0;
-
-
-
 
 int32_t last_signal = 0;
 int32_t last_send = 0;
@@ -23,6 +21,23 @@ volatile uint8_t active = 0;
 
 uint32_t current_code = 0, last_code = 0;
 uint8_t button_down = 0;
+
+void packet_sent(const uint8_t *mac_addr, esp_now_send_status_t status) {
+  wifi_sent++;
+//  Serial.println("ACK");
+//  Serial.flush();
+}
+
+void stop_wifi() {
+  if (wifi_state && wifi_sent>=2){
+  esp_now_deinit();
+  esp_wifi_stop();
+  esp_wifi_set_promiscuous(false);
+  WiFi.mode( WIFI_MODE_NULL );
+  adc_power_off();  // adc power off disables wifi entirely, upstream bug
+  wifi_state = 0;
+  }
+}
 
 
 void start_wifi() {
@@ -42,18 +57,8 @@ void start_wifi() {
   brcst.channel = channel;
   brcst.ifidx = ESP_IF_WIFI_STA;
   esp_now_add_peer(&brcst);
-  }
+  esp_now_register_send_cb(packet_sent);
   wifi_state = 1;
-}
-
-void stop_wifi() {
-  if (wifi_state && millis()-last_send>200){
-  esp_now_deinit();
-  esp_wifi_stop();
-  esp_wifi_set_promiscuous(false);
-  WiFi.mode( WIFI_MODE_NULL );
-  adc_power_off();  // adc power off disables wifi entirely, upstream bug
-  wifi_state = 0;
   }
 }
 
@@ -61,6 +66,7 @@ void stop_wifi() {
 void send_data() {
   if (data_to_send) {
     start_wifi();
+    wifi_sent=0;
     esp_now_send(broadcast_addr, packet_data, 2);
     esp_now_send(broadcast_addr, packet_data, 2);
     data_to_send = 0;
@@ -130,7 +136,7 @@ void IRAM_ATTR debug_button() {
 }
 
 void setup() {
-//      Serial.begin(115200);
+//      Serial.begin(921600);
 
   memset(&timing_chain, 0, sizeof(timing_chain));
 
@@ -147,8 +153,14 @@ void loop() {
 //  return;
 
   int32_t timing = (micros() - last_signal);
-  if (active && timing > 100000) {
-    //        Serial.print("active: "); Serial.print(active); Serial.print(", timing: "); Serial.print(timing); Serial.print(", counter: "); Serial.println(counter-marker);
+//Serial.print("a: "); Serial.print(active);
+//Serial.print(", t: "); Serial.print(timing);
+//Serial.print(", s: "); Serial.print(last_signal - delta_signal);
+//Serial.print(", d: "); Serial.print(data_to_send);
+//Serial.print(", w: "); Serial.print(wifi_state);
+//Serial.print(", c: "); Serial.println(counter-marker);
+//Serial.flush();
+  if (active && timing > 50000) {
     send_button_up(last_code);
 
     current_code = 0;
@@ -156,17 +168,21 @@ void loop() {
 
     active = 0;
     counter = 0;
+    marker=0;
     memset(&timing_chain, 0, sizeof(timing_chain));
   }
 
-  if (!active && timing > 500000 && !data_to_send && !wifi_state) {
+  if (!active && !data_to_send && !wifi_state) {
     esp_sleep_enable_timer_wakeup(5*60*(1000000)); //5min
     gpio_wakeup_enable(GPIO_NUM_33, GPIO_INTR_LOW_LEVEL );
     esp_sleep_enable_gpio_wakeup();
+//    Serial.println("L");
+//    Serial.flush();
     esp_light_sleep_start();
     
     esp_sleep_source_t wakeup_id = esp_sleep_get_wakeup_cause();
     if ( wakeup_id == ESP_SLEEP_WAKEUP_TIMER){ //timer wakeup!
+      esp_sleep_disable_wakeup_source(ESP_SLEEP_WAKEUP_ALL);
       esp_sleep_enable_ext0_wakeup(GPIO_NUM_33, 0);
       ////  esp_sleep_enable_ext1_wakeup((uint64_t)(1<<33),ESP_EXT1_WAKEUP_ALL_LOW);
       ////  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_SLOW_MEM, ESP_PD_OPTION_OFF);
@@ -174,6 +190,8 @@ void loop() {
       ////  esp_sleep_pd_config(ESP_PD_DOMAIN_RTC_PERIPH, ESP_PD_OPTION_ON);
       gpio_pulldown_dis(GPIO_NUM_33);
       gpio_pullup_en(GPIO_NUM_33);
+//    Serial.println("D");
+//    Serial.flush();
       esp_deep_sleep_start();
     }
   }
